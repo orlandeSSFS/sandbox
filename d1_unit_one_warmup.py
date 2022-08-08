af137c778b6cbdd339499b689f75b00189d942f6
orlandeSSFS <106611721+orlandeSSFS@users.noreply.github.com>
2022-08-08 14:53:34 -0400

    Add files via upload

---
+++ d1_unit_one_warmup.py
@@ -0,0 +1,18 @@
+import turtle
+
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+
+print("hello")
+
+for x in range(3):
+        turtle.left(120)
+        turtle.forward(100)
+
+
+
+turtle.exitonclick()
---
+++ spiro.py
@@ -0,0 +1,11 @@
+import turtle
+
+for x in range(8):
+    for x in range(3):
+        turtle.left(120)
+        turtle.forward(100)
+    turtle.forward(1)
+    turtle.left(30)
+
+
+turtle.exitonclick()
